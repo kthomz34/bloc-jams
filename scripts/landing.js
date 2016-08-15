a4fc63c19745360a4d743eb02f51383aa4004aa0
Kurt Thomas <kthomz34@icloud.com>
2016-08-15 18:31:18 +0000

    C9 events complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
-                
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
                 
     var revealPoint = function (index) {
         points[index].style.opacity = 1;
@@ -14,13 +14,18 @@ var animatePoints = function() {
     }
 };
 
-//var points = document.getElementsByClassName('point');
-//function revealPoint(points) {
-//    for (var i=0; i < points.length; i++){
-//        points[i].style.opacity = 1;
-//        points[i].style.transform = "scaleX(1) translateY(0)";
-//        points[i].style.msTransform = "scaleX(1) translateY(0)"
-//        points[i].style.WebkitTransform = "scale(1) translateY(0)"
-//    }
-//    revealPoint();
-//}
+window.onload = function() {
+    // Auto animate the points on a tall screen
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
+
