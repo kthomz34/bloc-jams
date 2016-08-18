021835acc9bfbc7a02c13c1576a063a9b7339c5c
Kurt Thomas <kthomz34@icloud.com>
2016-08-18 19:01:26 +0000

    A13 complete

--- scripts/album.js
+++ scripts/album.js
@@ -67,9 +67,15 @@ var setCurrentAlbum = function(album) {
 
 var findParentByClassName = function(element, targetClass) {
     if (element) {
-        var currentParent = element.parentElement;      
-        while (currentParent.className != targetClass && currentParent.className !== null) {
+        var currentParent = element.parentElement;
+        if (currentParent == null) {
+            alert("No parent found");
+        } else if (currentParent.className == null) {
+            alert("No parent found with that class name");
+        } else {
+            while (currentParent.className != targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
+            }
         }
         return currentParent;
     }
